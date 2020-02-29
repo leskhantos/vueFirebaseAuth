ffc33efbfbec1a9a414f0a4fa001fe052a8a7567
leskhantos <leskhan001@gmail.com>
2020-02-29 16:27:17 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -7,13 +7,7 @@ import 'firebase/firestore'
 
 Vue.config.productionTip = false
 const configOptions = {
-  apiKey: "AIzaSyCCsq8Y95GGZ9B9j_MgdwMYq0W6YGMUXRA",
-  authDomain: "blog-87b92.firebaseapp.com",
-  databaseURL: "https://blog-87b92.firebaseio.com",
-  projectId: "blog-87b92",
-  storageBucket: "blog-87b92.appspot.com",
-  messagingSenderId: "426891010188",
-  appId: "1:426891010188:web:14ea5137877edc117b5ba9"
+//
 }
 firebase.initializeApp(configOptions);
 firebase.auth().onAuthStateChanged(user => {
